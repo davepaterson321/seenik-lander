19d3c2f20dab13c9a29cede191a2b43879cb6bfd
Dave Paterson <davepaterson321@gmail.com>
2026-07-02 21:09:40 +0100

    updated eBay links

--- src/app/config.ts
+++ src/app/config.ts
@@ -4,25 +4,25 @@
 // CONTACT_EMAIL powers every secondary "Get in touch" mailto: link.
 export const EBAY_STORE_URL = 'https://ebay.us/m/VMkMEi';
 /** Jacobean backdrop listing on eBay. */
-export const EBAY_JACOBEAN_LISTING_URL = 'https://www.ebay.co.uk/itm/800030233150';
+export const EBAY_JACOBEAN_LISTING_URL = 'https://www.ebay.com/itm/800030233150';
 /** Stalingrad backdrop listing on eBay. */
 export const EBAY_STALINGRAD_LISTING_URL = 'https://ebay.us/m/jA6xZv';
 /** Napoleonic backdrop listing on eBay. */
 export const EBAY_NAPOLEONIC_LISTING_URL = 'https://ebay.us/m/KC53Gt';
 /** Gulf War backdrop listing on eBay. */
-export const EBAY_GULF_WAR_LISTING_URL = 'https://www.ebay.co.uk/itm/389968854720';
+export const EBAY_GULF_WAR_LISTING_URL = 'https://www.ebay.com/itm/389968854720';
 /** North Africa backdrop listing on eBay. */
-export const EBAY_NORTH_AFRICA_LISTING_URL = 'https://www.ebay.co.uk/itm/800094034721';
+export const EBAY_NORTH_AFRICA_LISTING_URL = 'https://www.ebay.com/itm/800094034721';
 /** Polar Fortress backdrop listing on eBay. */
-export const EBAY_POLAR_FORTRESS_LISTING_URL = 'https://www.ebay.co.uk/itm/389968713579';
+export const EBAY_POLAR_FORTRESS_LISTING_URL = 'https://www.ebay.com/itm/389968713579';
 /** Desert Citadel backdrop listing on eBay. */
 export const EBAY_DESERT_CITADEL_LISTING_URL = 'https://ebay.us/m/MvMozv';
 /** WW1 Western Front (Somme) backdrop listing on eBay. */
-export const EBAY_SOMME_LISTING_URL = 'https://www.ebay.co.uk/itm/800032689791';
+export const EBAY_SOMME_LISTING_URL = 'https://www.ebay.com/itm/800032689791';
 /** Eastern Europe / Crimea backdrop listing on eBay. */
-export const EBAY_CRIMEA_LISTING_URL = 'https://www.ebay.co.uk/itm/800031658636';
+export const EBAY_CRIMEA_LISTING_URL = 'https://www.ebay.com/itm/800031658636';
 /** French Town backdrop listing on eBay. */
-export const EBAY_FRENCH_TOWN_LISTING_URL = 'https://www.ebay.co.uk/itm/800204073072';
+export const EBAY_FRENCH_TOWN_LISTING_URL = 'https://www.ebay.com/itm/800204073072';
 export const CONTACT_EMAIL = 'hello@seenik.co.uk';
 
 export const CONTACT_MAILTO = `mailto:${CONTACT_EMAIL}`;
